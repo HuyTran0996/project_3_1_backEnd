28eb3e4cff2d481829e005f90c2459ba5222f048
HuyTran0996 <tranviquanghuy@gmail.com>
2023-12-29 15:11:18 +0700

    2023-12-29

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
